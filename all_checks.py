9fbb7c73c4ea8fedafbe909fa66e2422e2896fff
Alejo Acosta <alejoacos@gmail.com>
2020-10-25 17:49:35 +0000

    Added reboot check

--- all_checks.py
+++ all_checks.py
@@ -1,7 +1,13 @@
 #! /usr/bin/env python3
+import os
+
+def check_reboot():
+  """returns TRUE if a reboot is pending """
+  return os.path.exists("/run/reboot-required")
 
 def main():
   pass
 
 main()
+print ("reboot needed?: {}\n".format(check_reboot()))
 
